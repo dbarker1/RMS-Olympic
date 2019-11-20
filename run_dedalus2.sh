c21849932eaba6d0df77328c1b839a66e6063c5e
dbarker1 <57909238+dbarker1@users.noreply.github.com>
2019-11-20 15:46:57 +0000

    Removes files from pulled folder

--- run_dedalus2.sh
+++ run_dedalus2.sh
@@ -6,10 +6,12 @@ cores=$2
 git clone https://github.com/dbarker1/rm-history.txt
 mv rm-history.txt $name
 cd $name
+rm notes_RBconvection_script.pdf
 cp ~/run_param_file2.py .
 git checkout origin/Corrected_Rotation
 
 mpiexec -np $cores python3 $(pwd)/anelastic_RB.py
+rm -r __pycache__
 cp -r raw_data raw_data_cp
 
 merge.py raw_data/snapshots --cleanup

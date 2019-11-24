7d749c1b247feb3bdbd80f1c78d3d72a114d7a0a
dbarker1 <57909238+dbarker1@users.noreply.github.com>
2019-11-24 13:48:43 +0000

    Update run_w_github.py

--- run_w_github.py
+++ run_w_github.py
@@ -99,6 +99,6 @@ sp.call("merge.py " + cwd + "/raw_data/run_parameters --cleanup", shell=True)
 sp.call("merge_single.py " + name, shell=True)
 sp.call("plotting_snapshots.py " + name, shell=True)
 
-sp.call("mkdir -p ~/rm-history.txt/RESULTS" + param_path, shell=True)
+sp.call("mkdir -p ~/rm-history.txt/RESULTS/" + param_path, shell=True)
 sp.call("cd " + cwd + "/" + name + "_figs && cp -r * ~/rm-history.txt/RESULTS/", shell=True)
 sp.call("cd ~/rm-history.txt && git add . && git commit -m '" + name + "' && git push", shell=True)

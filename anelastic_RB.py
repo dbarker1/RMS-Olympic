d94ca1921ca6ca93206a9da5a4ffc053b0c74738
D Barker <danieljbarker@outlook.com>
2019-12-02 13:20:07 +0000

    Moved from master

--- anelastic_RB.py
+++ anelastic_RB.py
@@ -1,5 +1,3 @@
-## 2D RAYLEIGH-BENARD -- NO LONGER NEEDED FOR PROJECT
-
 """
 Dedalus script for 2D Anelastic Rayleigh-Benard convection.
 
@@ -33,9 +31,9 @@ import pathlib
 import logging
 logger = logging.getLogger(__name__)
 
-import run_param_file as rpf   # Imports a parameter file "run_param_file2.py"
+import run_param_file as rpf   # Imports a parameter file "run_param_file.py"
 
-save_direc = sys.argv[1] + "_raw_data/"
+save_direc = "raw_data/"
 pathlib.Path(save_direc).mkdir(parents=True, exist_ok=True)
 
 
@@ -45,8 +43,11 @@ Nx, Nz = rpf.Nx, rpf.Nz
 Pr = rpf.Pr
 Ra = rpf.Ra
 Np = rpf.Np
+Ta = rpf.Ta
+phi = rpf.latitude
 m = rpf.m
 theta = rpf.theta
+Roc = rpf.Roc
 
 # Create bases and domain
 x_basis = de.Fourier('x', Nx, interval=(0, Lx), dealias=3/2)   # Fourier basis in the x
@@ -55,7 +56,7 @@ domain = de.Domain([x_basis, z_basis], grid_dtype=np.float64)  # Defining our do
 z = domain.grid(1, scales=1)                                   # accessing the z values
 
 # 2D Anelastic hydrodynamics
-problem = de.IVP(domain, variables=['p', 's', 'u', 'w', 'sz', 'uz', 'wz', 'L_buoy', 'L_diss'])
+problem = de.IVP(domain, variables=['p', 's', 'u', 'v', 'vz', 'w', 'sz', 'uz', 'wz', 'L_buoy', 'L_diss'])
 problem.meta['p','s','u','w']['z']['dirichlet'] = True
 
 # Defining model parameters
@@ -63,10 +64,14 @@ problem.parameters['Lx'] = Lx
 problem.parameters['Lz'] = Lz
 problem.parameters['Ra'] = Ra
 problem.parameters['Pr'] = Pr
+problem.parameters['Ta'] = Ta
 problem.parameters['m'] = m
 problem.parameters['theta'] = theta
+problem.parameters['phi'] = phi
 problem.parameters['X'] = Ra/Pr
 problem.parameters['Y'] = (Pr*Pr*theta) / Ra
+problem.parameters['Roc'] = np.sqrt((Ra)/(Ta*Pr))
+
 
 # Non-constant coeffiecents
 rho_ref = domain.new_field(name='rho_ref')
@@ -86,27 +91,38 @@ problem.parameters['dz_rho_ref'] = dz_rho_ref   # z-derivative of rho_ref
 problem.add_equation("sz - dz(s) = 0")
 problem.add_equation("uz - dz(u) = 0")
 problem.add_equation("wz - dz(w) = 0")
+problem.add_equation("vz - dz(v) = 0")
+
+
 
 # mass continuity with rho_ref and dz(rho_ref) expanded analytically
 problem.add_equation("  (1-theta*z)*(dx(u) + wz) - theta*m*w = 0 ")
 
 # x-component of the momentum equation
-problem.add_equation("  rho_ref*( dt(u) - (4/3)*dx(dx(u)) - dz(uz) - (1/3)*dx(wz) ) + dx(p) \
+problem.add_equation("  rho_ref*( dt(u) - (4/3)*dx(dx(u)) - dz(uz) - (1/3)*dx(wz) - (Ta)**(0.5)*(v*sin(phi))  )  + dx(p) \
                         - dz_rho_ref*( uz + dx(w) ) \
-                        = -rho_ref*( u *dx(u) + w*uz ) ")
+                        = -rho_ref*(u*dx(u) + w*uz )   ")
+
+#y-component of the momentum equation
+problem.add_equation(" rho_ref*(dt(v) - dx(dx(v)) - dz(vz)  + (Ta)**(0.5)*(u*sin(phi) - w*cos(phi))  ) \
+                       - dz_rho_ref*(vz) \
+                       = -rho_ref*( u*dx(v) + w*vz )   ")
+
 
 # z-component of the momentum equation
-problem.add_equation("  rho_ref*T_ref*( dt(w) - X*s - (4/3)*dz(wz) - dx(dx(w)) - (1/3)*dx(uz) ) + T_ref*dz(p) + theta*m*p \
+problem.add_equation("  rho_ref*T_ref*( dt(w) - X*s - (4/3)*dz(wz) - dx(dx(w)) - (1/3)*dx(uz) + (Ta)**(0.5)*v*cos(phi) ) + T_ref*dz(p) + theta*m*p \
                         + (2/3)*theta*m*rho_ref*( 2*wz - dx(u) ) \
-                        = -rho_ref*T_ref*( u*dx(w) + w*wz ) ")
+                        = -rho_ref*T_ref*( u*dx(w) + w*wz )  ")
+
+
 # entropy diffusion equation
 problem.add_equation("  T_ref*( Pr*dt(s) - dx(dx(s)) - dz(sz) ) + theta*(m+1)*sz \
                         = -Pr*T_ref*( u*dx(s) + w*sz )    \
-                        + 2*Y*( dx(u)*dx(u) + wz*wz + uz*dx(w) - (1/3)*(dx(u)+wz)*(dx(u)+wz) + (1/2)*(uz*uz + dx(w)*dx(w))) ")
+                        + 2*Y*( dx(u)*dx(u) + wz*wz + uz*dx(w) - (1/3)*(dx(u)+wz)*(dx(u)+wz) + (1/2)*(uz*uz + dx(w)*dx(w) + dx(v)*dx(v) + vz*vz)) ")
 
 # Flux equations for use in analysis outputs
 problem.add_equation("  dz(L_buoy) = -s*rho_ref*w")
-problem.add_equation("  dz(L_diss) = -2*rho_ref*( (dx(u))**2 + wz**2 + (1/2)*( uz**2 + dx(w)**2 ) + dx(w)*uz - (1/3)*( dx(u) + wz )**2 )")
+problem.add_equation("  dz(L_diss) = -2*rho_ref*( dx(u)*dx(u) + wz*wz + uz*dx(w) - (1/3)*(dx(u)+wz)*(dx(u)+wz) + (1/2)*(uz*uz + dx(w)*dx(w) + dx(v)*dx(v) + vz*vz) )")
 
 problem.add_bc("left(w) = 0")            # Impermeable bottom boundary
 problem.add_bc("right(w) = 0", condition="(nx != 0)")   # Impermeable top boundary
@@ -115,9 +131,12 @@ problem.add_bc("left(uz) = 0")           # Stress-free bottom boundary
 problem.add_bc("right(uz) = 0")          # Stress-free top boundary
 problem.add_bc("right(s) = 0")           # Fixed entropy at upper boundary, arbitarily set to 0
 problem.add_bc("left(sz) = -1")          # Fixed flux at bottom boundary, F = F_cond
-
 problem.add_bc("left(L_buoy) = 0")       # BC for L_buoy for partial depth integration
 problem.add_bc("left(L_diss) = 0")       # BC for L_diss for partial depth integration
+problem.add_bc("right(vz) = 0")
+problem.add_bc("left(vz) = 0")
+
+
 
 # Build solver
 solver = problem.build_solver(de.timesteppers.RK222)
@@ -157,7 +176,7 @@ CFL.add_velocities(('u', 'w'))
 
 # Flow properties
 flow = flow_tools.GlobalFlowProperty(solver, cadence=10)
-flow.add_property("sqrt(u*u + w*w)", name='Re')
+flow.add_property("sqrt(u*u + w*w + v*v)", name='Re')
 
 # Saving snapshots
 snapshots = solver.evaluator.add_file_handler(save_direc + 'snapshots', sim_dt=rpf.snapshot_freq, max_writes=50)
@@ -166,9 +185,11 @@ snapshots.add_system(solver.state)
 # Analysis tasks
 analysis = solver.evaluator.add_file_handler(save_direc + 'analysis', sim_dt=rpf.analysis_freq, max_writes=5000)
 analysis.add_task("integ(s,'x')/Lx", layout='g', name='<s>_x')
+#analysis.add_task("integ(T,'x')/Lx", layout='g', name='<T>_x')
 
 # Mean Reynolds number
-analysis.add_task("integ( integ( sqrt(u*u + w*w) , 'x')/Lx, 'z')/Lz", layout='g', name='Re')
+analysis.add_task("integ( integ( sqrt(u*u + w*w + v*v) , 'x')/Lx, 'z')/Lz", layout='g', name='Re')
+
 
 # Flux decomposition - Internal energy equation
 analysis.add_task("integ(rho_ref*T_ref*s*w,'x')*Pr/Lx", layout='g', name='L_conv')
@@ -176,9 +197,9 @@ analysis.add_task("integ((-1)*rho_ref*T_ref*sz, 'x')/Lx", layout='g', name='L_co
 analysis.add_task("integ(L_buoy - interp(L_buoy,z=0),'x')*(-Pr*theta)/Lx", layout='g', name='L_buoy')
 analysis.add_task("integ(L_diss - interp(L_diss,z=0),'x')*((Pr*Pr*theta)/Ra)/Lx", layout='g', name='L_diss')
 
-# Flux decomposition - Total energy equaton (L_conv and L_cond already outputted)
-analysis.add_task("integ(0.5*rho_ref*(u*u + w*w)*w, 'x')*((Pr*Pr*theta)/Ra)/Lx", layout='g', name='L_KE')
-analysis.add_task("integ((-1)*rho_ref*(u*(uz + dx(w) ) \
+# Flux decomposition - Total energy equation (L_conv and L_cond already outputted)
+analysis.add_task("integ(0.5*rho_ref*(u*u + w*w + v*v)*w, 'x')*((Pr*Pr*theta)/Ra)/Lx", layout='g', name='L_KE')
+analysis.add_task("integ((-1)*rho_ref*( v*vz + u*(uz + dx(w)  ) \
                     + (2/3)*w*(2*wz - dx(u) )), 'x')*((Pr*Pr*theta)/Ra)/Lx", layout='g', name='L_visc')
 analysis.add_task("integ(p*w, 'x')*((Pr*Pr*theta)/Ra)/Lx", layout='g', name='L_p')
 
@@ -193,7 +214,7 @@ analysis.add_task(" (integ( integ( 2*rho_ref*( \
 analysis.add_task(" integ( (integ(rho_ref*T_ref*s*w,'x')/Lx)/T_ref, 'z')*Pr*theta/Lz ", layout='g', name='E_F_conv')
 
 # Mean KE
-analysis.add_task(" integ( (integ(0.5*(u*u + w*w)*rho_ref,'x')/Lx), 'z')/Lz", layout='g', name='KE')
+analysis.add_task(" integ( (integ(0.5*(u*u + w*w + v*v)*rho_ref,'x')/Lx), 'z')/Lz", layout='g', name='KE')
 
 # Creating a parameter file
 run_parameters = solver.evaluator.add_file_handler(save_direc + 'run_parameters', wall_dt=1e20, max_writes=1)
@@ -201,11 +222,13 @@ run_parameters.add_task(Lx, name="Lx")
 run_parameters.add_task(Lz, name="Lz")
 run_parameters.add_task(Ra, name="Ra")
 run_parameters.add_task(Pr, name="Pr")
+run_parameters.add_task(Ta, name="Ta")
 run_parameters.add_task(Np, name="Np")
 run_parameters.add_task(m,  name="m")
 run_parameters.add_task(Nx, name="Nx")
 run_parameters.add_task(Nz, name="Nz")
 run_parameters.add_task("z", layout='g', name="z_grid")
+run_parameters.add_task(Roc, name="Roc")
 
 run_parameters.add_task(rpf.snapshot_freq, name="snap_freq")
 run_parameters.add_task(rpf.analysis_freq, name="ana_freq")
@@ -223,7 +246,7 @@ try:
             # Prints various parameters to terminal upon starting the simulation
             logger.info('Parameter values imported form run_param_file.py:')
             logger.info('Lx = {}, Lz = {}; (Resolution of {},{})'.format(Lx, Lz, Nx, Nz))
-            logger.info('Ra = {}, Pr = {}, Np = {}'.format(Ra, Pr, Np))
+            logger.info('Ra = {}, Pr = {}, Np = {}, Ta = {}, phi = {}'.format(Ra, Pr, Np, Ta, phi))
             logger.info('Snapshot files outputted every {}'.format(rpf.snapshot_freq))
             logger.info('Analysis files outputted every {}'.format(rpf.analysis_freq))
             if rpf.end_sim_time != np.inf:

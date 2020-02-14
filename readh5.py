a386bdddd67501aafe9f7fc13f69063026554fa4
Daniel Barker <djb236@macphy-02.ex.ac.uk>
2020-02-14 11:59:31 +0000

    New branch

--- readh5.py
+++
@@ -1,9 +0,0 @@
-import h5py
-
-files = ["analysis", "run_parameters", "snapshots"]
-for file_name in files:
-    ana = h5py.File('raw_data/' + file_name + '/' + file_name + '_test1.h5', 'r')
-    print("Opened file " + file_name)
-    for ana_key in list(ana.keys()):
-        obj = ana[ana_key]
-        print("The shape of " + obj + " is " + str(obj.shape) + ".")

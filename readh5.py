35e8cfeaad1fdafe81392d39bb0b973883d861cc
D Barker <danieljbarker@outlook.com>
2020-02-16 18:50:50 +0000

    changed

--- readh5.py
+++ readh5.py
@@ -5,5 +5,5 @@ for file_name in files:
     ana = h5py.File('raw_data/' + file_name + '/' + file_name + '_test1.h5', 'r')
     print("Opened file " + file_name)
     for key in list(ana.keys()):
-        print(key)
+        print(ana[key])
         #print("The shape of " + key + " is " + str(ana[key].shape) + ".")

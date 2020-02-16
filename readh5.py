506c6b4ba3f17cfb5a03d0f0c0f522a0a06ce28a
D Barker <danieljbarker@outlook.com>
2020-02-16 19:13:29 +0000

    run name from cmd

--- readh5.py
+++ readh5.py
@@ -1,8 +1,11 @@
 import h5py
+import sys
+
+run_name = sys.argv[1]
 
 files = ["analysis", "run_parameters", "snapshots"]
 for file_name in files:
-    ana = h5py.File('raw_data/' + file_name + '/' + file_name + '_test1.h5', 'r')
+    ana = h5py.File('raw_data/' + file_name + '/' + file_name + '_' + run_name + '.h5', 'r')
     print("\nOpened file " + file_name)
     for key in list(ana.keys()):
         dataset = ana[key]

a9befbb65f483d8a043788034a87694413797878
D Barker <danieljbarker@outlook.com>
2020-02-16 19:12:47 +0000

    working readh5

--- readh5.py
+++ readh5.py
@@ -3,9 +3,8 @@ import h5py
 files = ["analysis", "run_parameters", "snapshots"]
 for file_name in files:
     ana = h5py.File('raw_data/' + file_name + '/' + file_name + '_test1.h5', 'r')
-    print("Opened file " + file_name)
+    print("\nOpened file " + file_name)
     for key in list(ana.keys()):
-        print(key)
-        for obj in list(ana[key]):
-            print(obj)
-        #print("The shape of " + key + " is " + str(ana[key].shape) + ".")
+        dataset = ana[key]
+        for obj in list(dataset):
+            print(dataset[obj])
